af03d52dbcdcaee15593efb9e1767e1a780a501e
uoiauai <113346725+uoiauai@users.noreply.github.com>
2022-10-31 22:00:25 +0800

    Update config.js

--- config.js
+++ config.js
@@ -12,14 +12,14 @@ const CONFIG = {
 
 	// General
 	name: 'John',
-	imageBackground: false,
+	imageBackground: true,
 	openInNewTab: true,
 	twelveHourFormat: false,
 
 	// Greetings
-	greetingMorning: 'Good morning!',
-	greetingAfternoon: 'Good afternoon,',
-	greetingEvening: 'Good evening,',
+	greetingMorning: 'Good Morning!',
+	greetingAfternoon: 'Good Afternoon!',
+	greetingEvening: 'Good Evening!',
 	greetingNight: 'Go to Sleep!',
 
 	// Layout
@@ -27,13 +27,13 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
-	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'Dark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
-	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
+	defaultLatitude: '22.4445',
+	defaultLongitude: '114.0222',
 
 	// Autochange
 	autoChangeTheme: true,
